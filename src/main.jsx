4e964a957520099d36f954fc428e390968aafade
Matheus Cruz <165737916+rlxmts@users.noreply.github.com>
2024-07-31 10:22:12 -0300

    :sparkles: Criando pastas e arquivos necessarios

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createGlobalStyle } from 'styled-components'
 
+const EstiloGlobal = createGlobalStyle`
+   *{
+     font-family: "Montserrat";
+     font-weight: 400;
+     padding: 0;
+     margin: 0;
+     text-decoration: none;
+     list-style: none;
+     box-sizing: border-box;
+     color: #000000;
+     font-size: 16px;
+   } 
+  `
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <h1>Olá mundo</h1>
-  </React.StrictMode>,
+    <EstiloGlobal />
+  </React.StrictMode>
 )
